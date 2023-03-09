b19db22064aede325c95fa02ae855af4ddb81dd2
Rod-code <ishimwerodrigue10@gmail.com>
2023-03-09 16:47:04 +0200

    intergration

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form');
-// const username = document.getElementById('username');
+const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -13,13 +13,41 @@ users = JSON.parse(localStorage.getItem('users')) || [];
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkInput();
-    signIn();
-    // if (isformValid() == true) {
-    //     form.submit();
-    // } else {
+    // signIn();
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const data = { email, password }
 
-    // }
+    // use fetch method to interact with your login api endpoint
+    fetch('https://dizzy-ruby-gilet.cyclic.app/api/v1/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            if (data.ok) {
+                // set our token in LS
+
+                localStorage.setItem("authToken", data.token)
+                location.href = "./dashboard.html"
+
+            } else {
+
+                alert(data.message)
+            }
+        })
+        .catch(err => alert(err))
 });
+// if (isformValid() == true) {
+//     form.submit();
+// } else {
+
+// }
+
 
 // function isformValid() {
 //     const divForm = form.querySelectorAll('div-form');
@@ -78,20 +106,20 @@ function isEmail(email) {
 }
 
 
-function signIn() {
-    // check if user exists
-    const targetUser = users.find(user => user.email == email.value);
-    if (targetUser && targetUser.password == password.value) {
-        localStorage.setItem('currentUser', JSON.stringify(targetUser))
-        window.location.href = "./dashboard.html";
-    } else if (targetUser && targetUser.password != password.value) {
-        alert('wrong password');
-        // } else {
-        //     // user doesn't exist
+// function signIn() {
+//     // check if user exists
+//     const targetUser = users.find(user => user.email == email.value);
+//     if (targetUser && targetUser.password == password.value) {
+//         localStorage.setItem('currentUser', JSON.stringify(targetUser))
+//         window.location.href = "./dashboard.html";
+//     } else if (targetUser && targetUser.password != password.value) {
+//         alert('wrong password');
+//         // } else {
+//         //     // user doesn't exist
 
-        //     window.location.href = './signup.html';
-        // }
-    }
-}
+//         //     window.location.href = './signup.html';
+//         // }
+//     }
+// }
 
-login.onclick = signIn
+// login.onclick = signIn
