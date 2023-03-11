97322890b14c19b5c4f69daf912cda254b0ccbf6
Rod-code <ishimwerodrigue10@gmail.com>
2023-03-11 18:24:20 +0200

    fixing the delete button

--- js/blogform.js
+++ js/blogform.js
@@ -167,7 +167,7 @@ fetchBlogs()
 async function deleteBlog(index) {
     console.log(index);
     // const token = JSON.parse(localStorage.getItem('token'));
-    let blogId = urlParams.get('id');
+    // let blogId = urlParams.get('id');
     await fetch(`http://localhost:6001/api/v1/blogs/${index}`, {
             method: "DELETE",
             headers: {
